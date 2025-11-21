8c13326635bbcb76c204ef95a600530840993152
jacqpat <82665557+jacqpat@users.noreply.github.com>
2025-11-21 15:52:41 +0100

    Update BB_keep_selected_microsats.py

--- BB_keep_selected_microsats.py
+++ BB_keep_selected_microsats.py
@@ -1,2 +1,9 @@
 import pandas as pd
 
+tsv=r"/PATH/TO/YOUR/GENOME/matrix.tsv"
+ssr=r"/PATH/TO/YOUR/SELECTED/microsats.tsv"
+out=r"/PATH/TO/YOUR/FILTERED/genome_matrix.tsv"
+df = pd.read_csv(tsv, sep="\t")
+targets = pd.read_csv(ssr, sep="\t", header=None, names=["CHROM","POS"])
+filtered = df.merge(targets, on=["CHROM","POS"])
+filtered.to_csv(out, sep="\t", index=False)
